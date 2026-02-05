f4c391b82f560c6afa1c29707cacef459bba6eee
Johan Viancha <vianchajohan@gmail.com>
2026-02-04 23:26:04 -0500

    ajustar la entidad de tareas

--- functions/src/domain/entities/task.entity.ts
+++ functions/src/domain/entities/task.entity.ts
@@ -5,14 +5,22 @@ export class Task {
     public description: string,
     public completed: boolean,
     public readonly userId: string,
-    public readonly createdAt: Date
+    public readonly createdAt: Date,
   ) {}
 
-  complete() {
-    this.completed = true;
+  static create(
+    id: string,
+    title: string,
+    descripcion: string,
+    userId: string,
+    createdAt: Date,
+  ) {
+    new Task(id, title, descripcion, false, userId, createdAt);
   }
 
-  markAsPending() {
-    this.completed = false;
+  update(title: string, descripcion: string, completed: boolean) {
+    this.title = title;
+    this.description = descripcion;
+    this.completed = completed;
   }
 }

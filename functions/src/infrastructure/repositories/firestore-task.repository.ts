26272def704238e30d580d85aaa29f8ee339f624
Johan Viancha <vianchajohan@gmail.com>
2026-02-04 23:27:37 -0500

    implementar repositorios para comunicarse con firestore

---
+++ functions/src/infrastructure/repositories/firestore-task.repository.ts
@@ -0,0 +1,65 @@
+import { TaskRepository } from "../../domain/repositories/task.repository";
+import { Task } from "../../domain/entities/task.entity";
+import { db } from "../firestore/firestore.client";
+
+export class FirestoreTaskRepository implements TaskRepository {
+  private collection = db.collection("tasks");
+
+  async findByUser(userId: string): Promise<Task[]> {
+    const snapshot = await this.collection
+      .where("userId", "==", userId)
+      .orderBy("createdAt", "desc")
+      .get();
+
+
+    return snapshot.docs.map((doc) => {
+      const data = doc.data();
+      return new Task(
+        doc.id,
+        data.title,
+        data.description,
+        data.completed,
+        data.userId,
+        data.createdAt.toDate(),
+      );
+    });
+  }
+
+  async findById(id: string): Promise<Task | null> {
+    const doc = await this.collection.doc(id).get();
+    if (!doc.exists) return null;
+
+    const data = doc.data()!;
+    return new Task(
+      doc.id,
+      data.title,
+      data.description,
+      data.completed,
+      data.userId,
+      data.createdAt.toDate(),
+    );
+  }
+
+  async create(task: Task): Promise<Task> {
+    await this.collection.doc().set({
+      title: task.title,
+      description: task.description,
+      completed: task.completed,
+      userId: task.userId,
+      createdAt: task.createdAt,
+    });
+    return task;
+  }
+
+  async update(task: Task): Promise<void> {
+    await this.collection.doc(task.id).update({
+      title: task.title,
+      description: task.description,
+      completed: task.completed,
+    });
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.collection.doc(id).delete();
+  }
+}
---
+++ functions/src/infrastructure/repositories/firestore-user.repository.ts
@@ -0,0 +1,30 @@
+import { UserRepository } from "../../domain/repositories/user.repository";
+import { User } from "../../domain/entities/user.entity";
+import { db } from "../firestore/firestore.client";
+
+export class FirestoreUserRepository implements UserRepository {
+  private collection = db.collection("users");
+
+  async findByEmail(email: string): Promise<User | null> {
+    const snapshot = await this.collection
+      .where("email", "==", email)
+      .limit(1)
+      .get();
+
+    if (snapshot.empty) return null;
+
+    const doc = snapshot.docs[0];
+    const data = doc.data();
+
+    return new User(data.id, data.email, data.createdAt.toDate());
+  }
+
+  async create(user: User): Promise<User> {
+    await this.collection.add({
+      email: user.email,
+      createdAt: user.createdAt,
+    });
+
+    return user;
+  }
+}

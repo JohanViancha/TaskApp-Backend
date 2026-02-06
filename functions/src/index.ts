9fde5ab8ba6b7861ccf1217f7751e56d20d863de
Johan Viancha <vianchajohan@gmail.com>
2026-02-06 16:53:18 -0500

    ajustar servidor

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import functions from "firebase-functions";
+import * as functions from "firebase-functions";
 import app from "./presentation/server";
 
 export const api = functions.https.onRequest(app);
--- functions/src/presentation/server.ts
+++ functions/src/presentation/server.ts
@@ -1,41 +1,22 @@
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import express from "express";
-import { AppRoutes } from "./routes";
+    import cookieParser from "cookie-parser";
+    import cors from "cors";
+    import express from "express";
+    import { AppRoutes } from "./routes";
 
-const app = express();
+    const app = express();
 
-app.use(
-  cors({
-    origin: "https://taskapp-1ffe0.web.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-);
-app.options(
-  "*",
-  cors({
-    origin: "https://taskapp-1ffe0.web.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-);
-app.use(express.json());
-app.use(cookieParser());
+    app.use(
+      cors({
+        origin: "https://taskapp-1ffe0.web.app",
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+      }),
+    );
+    
+    app.use(express.json());
+    app.use(cookieParser());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://taskapp-1ffe0.web.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-  return next();
-});
+    app.use(AppRoutes.routes);
 
-app.use(AppRoutes.routes);
-
-export default app;
+    export default app;

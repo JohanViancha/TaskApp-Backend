eac3768c07e0fd88fd2cfbaa27f4b35a0f190501
Johan Viancha <vianchajohan@gmail.com>
2026-02-06 15:29:37 -0500

    adjust origin

--- functions/src/presentation/server.ts
+++ functions/src/presentation/server.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:4200",
+    origin: "https://taskapp-1ffe0.web.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -16,7 +16,7 @@ app.use(
 app.options(
   "*",
   cors({
-    origin: "http://localhost:4200",
+    origin: "https://taskapp-1ffe0.web.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "https://taskapp-1ffe0.web.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
--- functions/src/presentation/user/router.ts
+++ functions/src/presentation/user/router.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
+import { CreateUserUseCase } from "../../application/use-cases/user/create-user.use-case";
+import { FirestoreUserRepository } from "../../infrastructure/repositories/firestore-user.repository";
 import { UserController } from "./controller";
 
 export class UserRoutes {
   static get routes(): Router {
     const router = Router();
 
-    const userController = new UserController();
+    const userRepository = new FirestoreUserRepository();
+    const createUser = new CreateUserUseCase(userRepository);
+    const userController = new UserController(createUser);
 
     router.post("/", userController.createUser);
 
